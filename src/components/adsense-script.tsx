0de2a286f2c7624040253a193e045cdf167372e8
Merthan Merter <merthanm@gmail.com>
2025-10-16 13:15:11 +0300

    fix adsense script

--- src/components/adsense-script.tsx
+++ src/components/adsense-script.tsx
@@ -1,12 +1,9 @@
-import Script from "next/script";
-
 export function AdSenseScript() {
 	return (
-		<Script
+		<script
 			async
-			src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9755830363670758`}
+			src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9755830363670758"
 			crossOrigin="anonymous"
-			strategy="afterInteractive"
 		/>
 	);
 }
